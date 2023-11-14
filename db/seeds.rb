91388db812365e3b31c113714fc00a8489381731
Emmanuel LeBouedec <elebouedec@gmail.com>
2023-11-14 16:09:30 +0100

    exercice 3 not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+require "faker"
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+  article.save
+end
 # Example:
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
